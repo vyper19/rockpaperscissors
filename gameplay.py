3259e0d46bc49bfbb06209bd99da24d1c2419bfd
vyper19 <103770064+vyper19@users.noreply.github.com>
2022-12-12 14:55:40 -0600

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -1,14 +1,30 @@
-import os
-import openai
+import random
+# print("Let's Play Rock, Paper, Scissors")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+player1 = input("[Player 1] Please choose Rock, Paper, Scissors: ")
+#Create a list of 3 items
+options = ["Rock", "Paper", "Scissors"]
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="write a python program for rock, paper, scissors \n\nimport random\n# print(\"Let's Play Rock, Paper, Scissors\")\n\nplayer1 = input(\"[Player 1] Please choose Rock, Paper, Scissors: \")\n#Create a list of 3 items\noptions = [\"Rock\", \"Paper\", \"Scissors\"]\n\n#Generate a random choice\nplayer2 = random.choice(options)\n\n#Print out the variables\n# print(f\"Player 1 chose {player1} and Player 2 chose {player2}!\")\n\nif player1 == player2:\n    print(\"It's a tie!\")\nelif player1 == \"Rock\":\n    if player2 == \"Scissors\":\n        print(\"Player 1 wins!\")\n    else:\n        print(\"Player 2 wins!\")\nelif player1 == \"Paper\":\n    if player2 == \"Rock\":\n        print(\"Player 1 wins!\")\n    else:\n        print(\"Player 2 wins!\")\nelif player1 == \"Scissors\":\n    if player2 == \"Paper\":\n        print(\"Player 1 wins!\")\n    else:\n        print(\"Player 2 wins!\")",
-  temperature=0.97,
-  max_tokens=1097,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+#Generate a random choice
+player2 = random.choice(options)
+
+#Print out the variables
+# print(f"Player 1 chose {player1} and Player 2 chose {player2}!")
+
+if player1 == player2:
+    print("It's a tie!")
+elif player1 == "Rock":
+    if player2 == "Scissors":
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+elif player1 == "Paper":
+    if player2 == "Rock":
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+elif player1 == "Scissors":
+    if player2 == "Paper":
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
